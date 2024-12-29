f3393acee83a07fcc5def89dcb9299d9985401c1
terrencepledger <terrencepledger@gmail.com>
2024-12-28 18:19:49 -0600

    separate logic to view model

---
+++ SlidesCast/ViewModels/PhotoGalleryViewModel.swift
@@ -0,0 +1,48 @@
+//
+//  PhotoGalleryViewModel.swift
+//  SlidesCast
+//
+//  Created by Terrence Pledger on 12/28/24.
+//
+
+import Foundation
+
+class PhotoGalleryViewModel: ObservableObject {
+    @Published var imgDetails: [ImageDetails] = []
+    @Published var selectedImages: [ImageDetails] = []
+    @Published var isSelectionMode: Bool = false
+    @Published var isAllSelected: Bool = false
+
+    private let photoLibrary: PhotoLibrary
+
+    init(photoLibrary: PhotoLibrary = PhotoLibrary()) {
+        self.photoLibrary = photoLibrary
+        self.photoLibrary.$imgDetails.assign(to: &$imgDetails)
+    }
+
+    func loadPhotos() {
+        photoLibrary.loadPhotos()
+    }
+
+    func toggleSelection(for img: ImageDetails) {
+        if let index = selectedImages.firstIndex(of: img) {
+            selectedImages.remove(at: index)
+        } else {
+            selectedImages.append(img)
+        }
+        updateSelectAllState()
+    }
+
+    func toggleSelectAll() {
+        if isAllSelected {
+            selectedImages.removeAll()
+        } else {
+            selectedImages = imgDetails
+        }
+        isAllSelected.toggle()
+    }
+
+    private func updateSelectAllState() {
+        isAllSelected = selectedImages.count == imgDetails.count
+    }
+}
