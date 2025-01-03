a2ed1a0111cec6155b1994ec208078dcff0225f7
terrencepledger <terrencepledger@gmail.com>
2025-01-03 12:23:46 -0600

    update appearance mode

--- SlidesCast/SlidesCastApp.swift
+++ SlidesCast/SlidesCastApp.swift
@@ -10,7 +10,9 @@ import SwiftData
 import GoogleCast
 
 @main
-struct SlidesCastApp: App {    
+struct SlidesCastApp: App {
+    @AppStorage(AppConfig.UserDefaults.appearance) private var appearanceMode: AppearanceMode = .systemDefault
+    
     var body: some Scene {
         WindowGroup {
             if isProduction {
@@ -21,12 +23,29 @@ struct SlidesCastApp: App {
                         GoogleSignInService.setup()
                     }
                     .withGlobalLoadingOverlay()
+                    .onChange(of: appearanceMode) {
+                        updateAppearance(mode: appearanceMode)
+                    }
             }
         }
     }
     
     private var isProduction: Bool {
         NSClassFromString("XCTest") == nil
+    }
+    
+    private func updateAppearance(mode: AppearanceMode) {
+        let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene
+        let keyWindow = windowScene?.windows.first(where: { $0.isKeyWindow })
+        
+        switch mode {
+        case .light:
+            keyWindow?.overrideUserInterfaceStyle = .light
+        case .dark:
+            keyWindow?.overrideUserInterfaceStyle = .dark
+        default:
+            keyWindow?.overrideUserInterfaceStyle = .unspecified
+        }
     }
 }
 
